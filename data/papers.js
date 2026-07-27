26e5962098db53f2841d00c90086432f226f480c
bodewits <26387260+bodewits@users.noreply.github.com>
2026-07-27 12:35:54 +0000

    Update recent ADS papers

--- data/papers.js
+++ data/papers.js
@@ -1,5 +1,5 @@
 window.LAB_ASTRO_PAPERS = {
-  "updated": "2026-07-20T11:57:34.887Z",
+  "updated": "2026-07-27T12:35:53.854Z",
   "source": "NASA ADS",
   "query": "(author:\"Fogle, M\" OR author:\"Loch, S\" OR author:\"Gall, A\" OR author:\"Bodewits, D\" OR author:\"Noonan, J\" OR author:\"Bromley, S\" OR author:\"Xing, Z\" OR author:\"Oset, S\") AND (aff:\"Auburn\" OR full:\"Auburn\")",
   "note": "Set ADS_API_TOKEN to refresh recent papers.",
